9b3f484158445ec67bcd9dffc832bdc3a70709dd
The_Hufaaz <ufasabdulmutolib@gmail.com>
2023-05-20 14:20:39 +0100

    added nginx default configuration file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('./connection')
 const server = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server, {
-  cors: 'http://localhost:3001',
+  cors: process.env.CLIENT_API,
   methods: ['GET', 'POST', 'PATCH', "DELETE"]
 })
 
